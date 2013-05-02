cc74ed9216f5c79ccdd088e5775f92a7d6d49237
andris310 <andris310@gmail.com>
2013-05-02 10:05:41 -0700

    Syntax adjustment

--- javascript.js
+++ javascript.js
@@ -1,39 +1,39 @@
+
+	function getInfo(){
+		var imdbID = $(this).attr('data-id');
+		$.ajax({
+			url: "http://www.omdbapi.com/?i=" + imdbID,
+			method: 'get',
+			dataType: 'jsonp',
+			success: function(info){
+
+				// get attributes 
+				var poster = ('<img src="' + info['Poster'] + '"/>');
+
+				// build details node
+				var details = $('<div id="details"></div>');
+				var textInfo = $('<div id="text_info"></div>');
+				details.append($("<h2>" + info['Title'] + " " + info['Year'] + "</h2>"));
+				details.append(poster);
+				textInfo.append($('<span><h4>Genre: </h4>' + info['Genre'] + '</span>'));
+				textInfo.append($('<span><h4>Director: </h4>' + info['Director'] + '</span>'));
+				textInfo.append($('<span><h4>Actors: </h4>' + info['Actors'] + '</span>'));
+				textInfo.append($('<p><h4>Plot: </h4>' + info['Plot'] + '</p>'));
+				details.append(textInfo);
+
+				// add node to DOM
+				$('#info').html(details);
+			}
+		});
+	}
+
 $(document).ready(function() {
 
 
 	$('#results_list').on('click', 'li', getInfo);
 
-		function getInfo(){
-			var imdbID = $(this).attr('data-id');
-			$.ajax({
-				url: "http://www.omdbapi.com/?i=" + imdbID,
-				method: 'get',
-				dataType: 'jsonp',
-				success: function(info){
-
-					// get attributes 
-					var poster = ('<img src="' + info['Poster'] + '"/>');
-
-					// build details node
-					var details = $('<div id="details"></div>');
-					var textInfo = $('<div id="text_info"></div>')
-					details.append($("<h2>" + info['Title'] + " " + info['Year'] + "</h2>"));
-					details.append(poster);
-					textInfo.append($('<span><h4>Genre: </h4>' + info['Genre'] + '</span>'));
-					textInfo.append($('<span><h4>Director: </h4>' + info['Director'] + '</span>'));
-					textInfo.append($('<span><h4>Actors: </h4>' + info['Actors'] + '</span>'));
-					textInfo.append($('<p><h4>Plot: </h4>' + info['Plot'] + '</p>'));
-					details.append(textInfo);
-
-					// add node to DOM
-					$('#info').html(details);
-				}
-			});
-		}
-
 	$('form').bind('keyup submit', function(event) {
 		event.preventDefault();
-		$('#results_list').fadeIn(400);
 		var title = $('#movie_title').val();
 
 		$.ajax({
